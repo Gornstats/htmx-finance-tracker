03ff6506d199a92e2766a900b63175ab966b7949
Jeremy Gorniak <railspider@gmail.com>
2024-06-10 18:46:31 +1000

    added totals to transaction screen

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from tracker.models import Category, Transaction
+from tracker.models import Category, Transaction, User
 
 # Register your models here.
 admin.site.register(Transaction)
 admin.site.register(Category)
+admin.site.register(User)
