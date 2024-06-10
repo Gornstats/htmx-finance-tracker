2c73f50c4a24cd57344faa874ef81ae52d23d8d2
Jeremy Gorniak <railspider@gmail.com>
2024-06-10 17:56:33 +1000

    created tests

--- tracker/tests/conftest.py
+++ tracker/tests/conftest.py
@@ -4,4 +4,4 @@ from tracker.factories import TransactionFactory
 # fixture sets up a consistent state for testing, and removes this data once tests are complete
 @pytest.fixture
 def transactions():
-    TransactionFactory.create_batch(25)
+    return TransactionFactory.create_batch(25)
